ac0c907d30c78ff311ef04ffd090b871eb3498c9
Sushanthashok <sushanthashok191@gmail.com>
2026-01-21 22:23:05 +0530

    Add files via upload

---
+++ ASCIIValues.java
@@ -0,0 +1,9 @@
+public class ASCIIValues {
+    public static void main(String[] args) {
+
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.println(ch + " : " + (int) ch);
+        }
+    }
+}
+
